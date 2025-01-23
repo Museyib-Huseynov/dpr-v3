11b80127342b610c24821a705b8dd1000359d683
Museyib Huseynov <museyib.huseynov.dev@gmail.com>
2025-01-23 19:17:13 +0400

    logic for joins corrected, db updated with correct view

--- view.sql
+++ view.sql
@@ -32,7 +32,7 @@ FROM daily_well_parameters dwp
 LEFT JOIN well_stock AS ws
 ON dwp.well_id = ws.well_id
 AND ws.report_date_id = (
-	SELECT MAX(ws.report_date_id)
+	SELECT MAX(ws_sub.report_date_id)
 	FROM well_stock AS ws_sub
 	WHERE ws_sub.well_id = dwp.well_id
 	AND ws_sub.report_date_id <= dwp.report_date_id
@@ -40,7 +40,7 @@ AND ws.report_date_id = (
 LEFT JOIN completions AS c
 ON dwp.well_id = c.well_id
 AND c.report_date_id = (
-	SELECT MAX(c.report_date_id)
+	SELECT MAX(c_sub.report_date_id)
     FROM completions AS c_sub
     WHERE c_sub.well_id = dwp.well_id
     AND c_sub.report_date_id <= dwp.report_date_id
@@ -48,7 +48,7 @@ AND c.report_date_id = (
 LEFT JOIN well_downtime_reasons AS wdr
 ON dwp.well_id = wdr.well_id
 AND wdr.report_date_id = (
-	SELECT MAX(wdr.report_date_id)
+	SELECT MAX(wdr_sub.report_date_id)
     FROM well_downtime_reasons AS wdr_sub
     WHERE wdr_sub.well_id = dwp.well_id
     AND wdr_sub.report_date_id <= dwp.report_date_id
